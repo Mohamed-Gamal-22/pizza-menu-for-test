09dfa8042809be2ac7b8718b7d3d292db79f19a5
mohamed gamal <mogamall086@gmail.com>
2023-09-11 22:43:00 +0300

    add date open and close

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -4,6 +4,10 @@ import Pizza from "../Pizza/Pizza.jsx";
 
 export default class Menu extends Component {
   render() {
+    let hour = new Date().getHours();
+    let openHour = 12;
+    let cloesHour = 22;
+    const isOpen = hour >= openHour && hour < cloesHour;
     console.log(pizzaData);
     return (
       <>
@@ -12,12 +16,27 @@ export default class Menu extends Component {
             {pizzaData.map((pizza) => (
               <Pizza pizza={pizza} key={pizza.ingredients} />
             ))}
-            <p className="mt-5">
-              We're open from 12:00 to 22:00. Come visit us or order online.
-            </p>
-            <div className="text-center">
-              <div className="btn btn-danger px-5 py-2">order</div>
-            </div>
+            {isOpen ? (
+              <>
+                <p className="mt-5 text-success fw-bold">
+                  We are open, Come visit us or order online.
+                </p>
+                <div className="text-center">
+                  <div className="btn btn-danger px-5 py-2 text-capitalize">
+                    order now !
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <p className="mt-5 mb-0 text-danger fw-bold">
+                  We're Closed For Now !
+                </p>
+                <p className="text-danger fw-bold">
+                We Happy to serve you  from 12:00PM to 22:00PM. 
+                </p>
+              </>
+            )}
           </div>
         </div>
       </>
