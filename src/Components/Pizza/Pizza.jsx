f7d02111a950e401f5c5bd97a703189805871be0
mohamed gamal <mogamall086@gmail.com>
2023-09-16 02:23:43 +0300

    add soldout

--- src/Components/Pizza/Pizza.jsx
+++ src/Components/Pizza/Pizza.jsx
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 
 export default class Pizza extends Component {
   render() {
-    let { ingredients, photoName, name, price } = this.props.pizza;
+    let { ingredients, photoName, name, price, soldOut } = this.props.pizza;
     return (
       <div className="col-lg-3 col-md-4 col-sm-6">
-        <div className="item bg-light p-2 rounded">
+        <div
+          className={
+            soldOut ? "item bg-light p-2 rounded" : "bg-light p-2 rounded pointer"
+          }
+        >
           <img className="w-100 mb-3 rounded" src={photoName} alt="pizza" />
           <h5 className="fw-bold text-uppercase text-danger">{name}</h5>
           <p className="small">{[ingredients]}</p>
-          <p className="small fw-bold text-success">price : {price} $</p>
+          {soldOut ? (
+            <p className="small fw-bold text-danger">SOLD OUT</p>
+          ) : (
+            <p className="small fw-bold text-primary"> price : {price}</p>
+          )}
         </div>
       </div>
     );
